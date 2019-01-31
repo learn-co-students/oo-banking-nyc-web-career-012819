7a98af3308f233b452b6ce5eac0370ebdbe0d1d7
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-01-31 11:31:00 -0500

    dolma

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(balance)
+    @balance += balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+      self.status = "closed"
+  end
+
 end
